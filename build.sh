b2211f03c85472e832b84b3570e6e7e0a362f8b1
Nizel Shirogane Lynee <104589134+sudo-do@users.noreply.github.com>
2023-06-27 15:09:51 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ echo "Torghost installer v3.0"
 echo "Installing prerequisites "
 sudo apt-get install tor python3-pip -y 
 echo "Installing dependencies "
-sudo pip3 install -r requirements.txt 
+pip3 install -r requirements.txt 
 mkdir build
 cd build
 cython3 ../torghost.py --embed -o torghost.c --verbose
